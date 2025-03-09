9e5de021701a806c1be12a61f598f33a8333d5b2
Ubuntu <ubuntu@ip-10-0-10-63.ec2.internal>
2025-03-09 16:01:35 +0000

    m

--- sql/database.sql
+++ sql/database.sql
@@ -23,3 +23,4 @@ INSERT INTO servicios_eventos (tipo_evento, nombre_servicio, encargado, contacto
 ('Diseño Interior', 'Decoración de oficinas', 'Ricardo Suárez', 'ricardo@example.com', 2500.00, 'Diseño innovador para oficinas y espacios de trabajo.'),
 ('Cumpleaños', 'Mesa de postres', 'Ana González', 'ana@example.com', 400.00, 'Mesa de postres y pasteles personalizados para cumpleaños.'),
 ('Boda', 'Fotografía y video', 'Daniel Pérez', 'daniel@example.com', 1500.00, 'Fotografía y videografía profesional para bodas.');
+
